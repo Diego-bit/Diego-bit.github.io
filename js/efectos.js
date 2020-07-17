7e7ffc998a1e597323f9f936512fc9380607a11c
Diego-bit <ugartediegoedgar@gmail.com>
2020-07-17 00:24:35 -0500

    actualizacion

--- js/efectos.js
+++ js/efectos.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
   $("#btn-contacto").on("click", function (e) {
     e.preventDefault;
     $("html, body").animate({
-      scrollTop: contacto,
+      scrollTop: contacto - 200,
     }),
       500;
   });
